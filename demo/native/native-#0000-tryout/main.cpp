e18b8476e1780819762f39fde63ef41dfdff3ec6
Wouter van Ooijen <wouter@voti.nl>
2021-03-16 21:05:40 +0100

    update

--- demo/native/native-#0000-tryout/main.cpp
+++ demo/native/native-#0000-tryout/main.cpp
@@ -18,7 +18,7 @@ struct resource_root {
 
 template< typename T >
 concept resource = requires {
-   std::derived_from< T, resource_root >;
+   { std::derived_from< T, resource_root > == true };
 };
 
 
@@ -67,8 +67,8 @@ struct list<> : resource_root {
 // a list of at least one resource
 template< resource _first, resource... _tail >
 struct list< _first, _tail... > {
-   using next   = list< _tail... >;
    using first  = _first;
+   using next   = list< _tail... >;
    
    static constexpr auto n_threads = first::n_threads + next::n_threads;
    
@@ -114,6 +114,7 @@ struct blink { template< typename application > struct main {
 
 // =============================================================================
 
+/*
 struct app { template< typename application > struct main {    
     
    using resources = list< 
@@ -141,10 +142,36 @@ template< typename application >
 void run(){
     run_class< application >::run();
 }
-
+*/
 
 // =============================================================================
 
-int main(){
-   run< app >();
+template< resource x >
+struct s {};
+
+int smain(){
+   //run< app >();
+   s< bool > a;
+   (void) a;
+   return 0;
+}
+
+class A {};
+ 
+class B: public A {};
+ 
+class C: private A{};
+ 
+int main() {
+    // std::derived_from == true only for public inheritance or exact same class
+    static_assert( std::derived_from<B, B> == true );      // same class: true
+    static_assert( std::derived_from<int, int> == false ); // same primitive type: false
+    static_assert( std::derived_from<B, A> == true );      // public inheritance: true
+    static_assert( std::derived_from<C, A> == false );     // private inheritance: false
+ 
+    // std::is_base_of == true also for private inheritance
+    static_assert( std::is_base_of_v<B, B> == true );      // same class: true
+    static_assert( std::is_base_of_v<int, int> == false ); // same primitive type: false
+    static_assert( std::is_base_of_v<A, B> == true );      // public inheritance: true
+    static_assert( std::is_base_of_v<A, C> == true );      // private inheritance: true
 }
